173ffdfd865bb88a613a7e23af858cf803b25ce4
Richa Agrawal <agrawalricha013@gmail.com>
2024-03-19 13:52:21 +0530

    dwfgyugfd

--- PowerLudo/Assets/Scripts/Controllers/AuthController.cs
+++ PowerLudo/Assets/Scripts/Controllers/AuthController.cs
@@ -31,7 +31,7 @@ public class AuthController : MonoBehaviour
             WelcomePanel.SetActive(false);
             AuthPanel.SetActive(false);
 
-            verificationController.numberText.text = "Hello: " + phoneNumber;
+            verificationController.numberText.text = $"{phoneNumber}";
         }
         else
         {
