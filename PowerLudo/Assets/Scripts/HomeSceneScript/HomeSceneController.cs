e9a61c657e6066e453af62b74d38d13f5c7f92e2
Bhukar01 <bhukarmamta98@gmail.com>
2024-04-01 18:06:51 +0530

    nbvcxdrt

--- PowerLudo/Assets/Scripts/HomeSceneScript/HomeSceneController.cs
+++ PowerLudo/Assets/Scripts/HomeSceneScript/HomeSceneController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class HomeSceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject HomePanel;
+    [SerializeField] private GameObject MoreInfoPanel;
+    [SerializeField] private GameObject MiniWalletPanel;
+    Public Button[] buttons;
+
+
+{
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+}
 }
