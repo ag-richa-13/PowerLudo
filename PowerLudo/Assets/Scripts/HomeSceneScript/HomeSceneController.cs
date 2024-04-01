0bf7d0857a2bf2b34182a01e39f369670943c85b
Bhukar01 <bhukarmamta98@gmail.com>
2024-04-01 18:23:05 +0530

    mnbvcxdfg

--- PowerLudo/Assets/Scripts/HomeSceneScript/HomeSceneController.cs
+++ PowerLudo/Assets/Scripts/HomeSceneScript/HomeSceneController.cs
@@ -9,6 +9,14 @@ public class HomeSceneController : MonoBehaviour
     [SerializeField] private GameObject MoreInfoPanel;
     [SerializeField] private GameObject MiniWalletPanel;
     public Button[] buttons;
+    void Start()
+    {
+
+        HomePanel.SetActive(true);
+        MoreInfoPanel.SetActive(false);
+        MiniWalletPanel.SetActive(false);
+
+    }
 
 
 
