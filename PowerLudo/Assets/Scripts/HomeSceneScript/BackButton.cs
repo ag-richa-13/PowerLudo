5d562c8ea65434fec632a22878255ba388a97d99
Richa Agrawal <agrawalricha013@gmail.com>
2024-04-05 10:36:26 +0530

    wertgvb

--- PowerLudo/Assets/Scripts/HomeSceneScript/BackButton.cs
+++ PowerLudo/Assets/Scripts/HomeSceneScript/BackButton.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BackButton : MonoBehaviour
 {
+    public HomeSceneController homeSceneController;
     [SerializeField] private GameObject HomePanel;
     [SerializeField] private GameObject MoreInfoPanel;
     [SerializeField] private GameObject WalletPanel;
@@ -23,15 +24,24 @@ public class BackButton : MonoBehaviour
             else if (MoreInfoPanel.activeSelf)
             {
 
-                MoreInfoPanel.SetActive(false);
-                HomePanel.SetActive(true);
-                WalletPanel.SetActive(false);
+                homeSceneController.OnClickHomeButton();
+                // MoreInfoPanel.SetActive(false);
+                // HomePanel.SetActive(true);
+                // WalletPanel.SetActive(false);
+
+                // buttons[3].gameObject.SetActive(true);
+                // buttons[0].gameObject.SetActive(true);
+                // buttons[4].gameObject.SetActive(true);
+                // buttons[1].gameObject.SetActive(false);
+                // buttons[5].gameObject.SetActive(false);
+                // buttons[2].gameObject.SetActive(false);
             }
             else if (WalletPanel.activeSelf)
             {
-                MoreInfoPanel.SetActive(false);
-                HomePanel.SetActive(true);
-                WalletPanel.SetActive(false);
+                homeSceneController.OnClickHomeButton();
+                // MoreInfoPanel.SetActive(false);
+                // HomePanel.SetActive(true);
+                // WalletPanel.SetActive(false);
             }
         }
     }
